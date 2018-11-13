4b8223570ceb359d9b9bcabf512622fc0c9ac643
programmingprincess <jiaqiwu@asu.edu>
2018-11-13 01:31:23 -0600

    complete histology counts

--- total_counts.R
+++ total_counts.R
@@ -20,13 +20,78 @@ trimSamples <- function(cancerSet, npar=TRUE, print=TRUE) {
   return(newLabels)
 }
 
+getData <- function (file, print=TRUE) {
+  temp_data = read.table(file, header=TRUE, stringsAsFactors=FALSE)
+  colnames(temp_data) = trimSamples(temp_data)
+  
+  row.names(temp_data) = temp_data$miRNA
+  temp_data = temp_data[-1] #trim repeated rownames 
+  
+  return(temp_data)
+}
 ## BLCA: bladder 
-blca_data = read.table("miR_counts_blca.txt", header=TRUE, stringsAsFactors=FALSE)
-colnames(blca_data) = trimSamples(blca_data)
-dim(blca_data)
-# [1] 2186  438
-# 2186 miRNAs in 438 samples 
-row.names(blca_data) = blca_data$miRNA
-blca_data = blca_data[-1]
+blca_data = getData("miR_counts_blca.txt")
+ncol(blca_data)   # number of samples 
+#nrow(blca_data)   # number of miRNAs
+
+## BRCA: breast 
+brca_data = getData("miR_counts_brca.txt")
+ncol(brca_data)
+
+## CHOL: bile duct 
+chol_data = getData("miR_counts_chol.txt")
+ncol(chol_data)
+
+## ESCA: esophagus 
+esca_data = getData("miR_counts_esca.txt")
+ncol(esca_data)
+
+## HNSC: head and neck 
+hnsc_data = getData("miR_counts_hnsc.txt")
+ncol(hnsc_data)
+
+## KICH: kidney chromophobe 
+kich_data = getData("miR_counts_kich.txt")
+ncol(kich_data)
+
+## KIRC: kidney renal clear cell 
+kirc_data = getData("miR_counts_kirc.txt")
+ncol(kirc_data)
+
+## LICH: liver 
+lich_data = getData("miR_counts_lich.txt")
+ncol(lich_data)
+
+## LUAD: lung 
+luad_data = getData("miR_counts_luad.txt")
+ncol(luad_data)
+
+## PRAD: prostate 
+prad_data = getData("miR_counts_prad.txt")
+ncol(prad_data)
+
+## STAD: stomach 
+stad_data = getData("miR_counts_stad.txt")
+ncol(stad_data)
+
+## THCA: thyroid 
+thca_data = getData("miR_counts_thca.txt")
+ncol(thca_data)
+
+## UCEC: uterus 
+ucec_data = getData("miR_counts_ucec.txt")
+ncol(ucec_data)
 
 
+#####
+## PAAD: pancreas 
+paad_data = getData("miR_counts_paad.txt")
+ncol(paad_data)
+
+## SKCM: skin melanoma 
+skcm_data = getData("miR_counts_skcm.txt")
+ncol(skcm_data)
+
+## OV: ovary 
+ov_data = getData("miR_counts_ov.txt")
+ncol(ov_data)
